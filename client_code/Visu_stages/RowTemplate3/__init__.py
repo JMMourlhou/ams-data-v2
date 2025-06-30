eab731e311cf25558c055fe0a25ad529b3d04bc8
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2025-06-30 14:59:49 +0000

    Edited Visu_stages.RowTemplate3, Recherche_stagiaire.RowTemplate1 and settings; Deleted InputBox

--- client_code/Visu_stages/RowTemplate3/__init__.py
+++ client_code/Visu_stages/RowTemplate3/__init__.py
@@ -8,7 +8,7 @@ from anvil.tables import app_tables
 from .. import Visu_stages
 from anvil import open_form
 from ...Pre_R_pour_stagiaire_admin import Pre_R_pour_stagiaire_admin
-from ... InputBox import input_box, alert2
+from InputBox.input_box import InputBox, alert2, input_box
 #import anvil.js    # pour screen size
 from anvil.js import window # to gain access to the window object
 global screen_size
