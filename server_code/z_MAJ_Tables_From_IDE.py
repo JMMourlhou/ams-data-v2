517ee5ad0eb83c91b2085bbbe7c938c7f6c33cba
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2025-07-28 14:06:37 +0000

    Created z_MAJ_Tables_From_IDE; Deleted ztest

---
+++ server_code/z_MAJ_Tables_From_IDE.py
@@ -0,0 +1,6 @@
+from anvil.tables import app_tables
+import anvil.server
+
+@anvil.server.callable
+def maj_tables():
+    return 
--- server_code/ztest.py
+++
@@ -1,7 +0,0 @@
-import anvil.tables as tables
-from anvil.tables import app_tables
-import anvil.server
-
-@anvil.server.callable
-def get_cpt_columns():
-    return [col["name"] for col in app_tables.cpt_stages._table.columns]
