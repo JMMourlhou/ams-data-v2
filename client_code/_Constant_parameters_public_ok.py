02c0e8f11dc17417a7ae568ce4998089064c9e0b
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2025-08-04 07:52:54 +0000

    Edited _Constant_parameters_public_ok

--- client_code/_Constant_parameters_public_ok.py
+++ client_code/_Constant_parameters_public_ok.py
@@ -1,6 +1,6 @@
 import anvil.server
 
-mon_logo = "logo_fs_small.png"
+mon_logo = "logo_F_S_small.png"
 ams_logo = "Logo_AMS.png"
 ams_en_tete = "en_tete_small.png"
 carte_ams = "carte_ams_small.jpg"
