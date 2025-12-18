b8955d713c5432247a07cfa15875cb7ea9bc07ed
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2025-12-18 19:03:18 +0000

    Edited Video_Player and html_video

--- client_code/Video_Player/__init__.py
+++ client_code/Video_Player/__init__.py
@@ -1,21 +1,16 @@
 from ._anvil_designer import Video_PlayerTemplate
 from anvil import *
-import anvil.js
 
 class Video_Player(Video_PlayerTemplate):
     def __init__(self, **properties):
         self.init_components(**properties)
+        self._sync()
 
-        # Accès au DOM du Custom HTML
-        self._root = self.html_video_1.dom_node
-        
-        # Si une URL est déjà définie, on charge la vidéo
-        if self.video_url:
-            self._apply_video_url(self.video_url)
+    def _sync(self):
+        # Réinjecte l’URL ET force le re-render du HTML
+        self.html_video_1.video_url = self.video_url
+        self.html_video_1.visible = False
+        self.html_video_1.visible = True
 
-    def _apply_video_url(self, url):
-        anvil.js.window._vp_set_src(self._root, url or "")
-
-    # Appelé automatiquement par Anvil quand video_url change
     def video_url_changed(self, **event_args):
-        self._apply_video_url(self.video_url)
+        self._sync()
