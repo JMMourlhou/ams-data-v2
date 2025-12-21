97deb0ad6905a43bbf6c7896930cff345d25e16a
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2025-12-21 16:33:26 +0000

    Edited Video_Player and Video_html

--- client_code/Video_Player/__init__.py
+++ client_code/Video_Player/__init__.py
@@ -4,5 +4,5 @@ from anvil import *
 class Video_Player(Video_PlayerTemplate):
     def __init__(self, **properties):
         self.init_components(**properties)
-
+        
         self.video_html_1.video_url = "https://media.jmweb34.net/Essai1_stream.mp4"
