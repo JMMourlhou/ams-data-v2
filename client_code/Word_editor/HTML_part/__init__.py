e8308e4659f822a58b8a8ab84373ab355e619b2f
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2025-12-31 18:21:16 +0000

    Edited Word_editor, QCM_visu_modif_html, QCM_visu_creation_html and Word_editor.HTML_part

--- client_code/Word_editor/HTML_part/__init__.py
+++ client_code/Word_editor/HTML_part/__init__.py
@@ -7,4 +7,5 @@ class HTML_part(HTML_partTemplate):
     def __init__(self, html_text="", **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
+        self.border = "1px solid #33a1b8"
 
