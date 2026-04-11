ec2febeefa72b9c693309bc0ce297635be10f72f
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2026-04-11 20:12:28 +0000

    Edited z_user_api_sign_in_search

--- server_code/z_user_api_sign_in_search.py
+++ server_code/z_user_api_sign_in_search.py
@@ -3,16 +3,11 @@ from anvil.files import data_files
 import anvil.email
 import anvil.users
 
-
-
-#import tables
-
 from anvil.tables import app_tables
 #from tables import app_tables
 #import anvil.tables as tables
 
 import anvil.tables.query as q
-#from anvil.tables import app_tables
 import anvil.server
 import bcrypt # to test the API key crypted
 from . import French_zone
