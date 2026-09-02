b65af28c1b555d89578e80240853d2017e3ce726
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2026-09-02 09:43:24 +0000

    Edited Main and Stage_visu_modif.RowTemplate4

--- client_code/Stage_visu_modif/RowTemplate4/__init__.py
+++ client_code/Stage_visu_modif/RowTemplate4/__init__.py
@@ -114,17 +114,17 @@ class RowTemplate4(RowTemplate4Template):
                     style="error",
                     large = True
                 )
-            if not r :   #non
-                if sov is False: # je remets à True
-                    self.check_box_form_suivi.checked = True
-                else:
-                    self.check_box_form_suivi.checked = False
-                return
+                if not r :   #non
+                    if sov is False: # je remets à True
+                        self.check_box_form_suivi.checked = True
+                    else:
+                        self.check_box_form_suivi.checked = False
+                    return
                 
             valid = anvil.server.call("init_formulaire_suivi_stagiaire", stagiaire_row, self.check_box_form_suivi.checked, True)   # on effacera le formulaire 
             if valid is True:
-                AlertHTML.success("Succès :","Le formulaire a bien été effacé !\n\n Il peut être ré-entré par le stagiaire si nécessaire.")
-        else: # stage Tuteur, AAN/MTnoto, je peux effacer le check du formulaire de suivi car chaque année il peut le refaire pour un autre stage
+                AlertHTML.success("Succès :","Le formulaire a bien été effacé !\n\n Il peut être ré-entré par le Tuteur si nécessaire.")
+        else: # stage Stagiaire, AAN/MTnoto, je peux effacer le check du formulaire de suivi car chaque année il peut le refaire pour un autre stage
             valid = anvil.server.call("init_formulaire_suivi_stagiaire", stagiaire_row, self.check_box_form_suivi.checked, False)   # False, on n'effacera pas les anciens formulaires
             if valid is True:
                 AlertHTML.success("Succès :","Réinitialisation effectuée au niveau du Tuteur, Le ou les formulaires n'ont pas été effacé(s). \n\n Vous pouvez les effacer dans la partie 'Paramètres'")
