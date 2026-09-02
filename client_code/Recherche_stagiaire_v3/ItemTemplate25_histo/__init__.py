cd9a40224a24449ee9466d0605f49a3072080002
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2026-09-02 13:16:52 +0000

    Edited Recherche_stagiaire_v3.ItemTemplate25_histo and Add_del_stagiaire

--- client_code/Recherche_stagiaire_v3/ItemTemplate25_histo/__init__.py
+++ client_code/Recherche_stagiaire_v3/ItemTemplate25_histo/__init__.py
@@ -11,12 +11,15 @@ class ItemTemplate25_histo(ItemTemplate25_histoTemplate):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
         # Any code you write here will run before the form opens.
-        if self.item['user_email']['role']=="S": 
-            self.button_detail_histo.text = self.item['stage']['code']['code'] +" du " + str(self.item['stage']['date_debut'])
-        else:
-            self.button_detail_histo.text = self.item['stage']['code']['code']
-            if self.item['numero']<1000:
-                self.button_detail_histo.text = self.item['stage']['code']['code'] +" du " + str(self.item['stage']['date_debut'])
+        
+        # Par défaut
+        self.button_detail_histo.text = self.item['stage']['code']['code'] 
+        
+        if self.item['user_email']['role'] == "S": 
+            self.button_detail_histo.text =  f"{self.item['stage']['code']['code']} du {str(self.item['stage']['date_debut'])} / #{self.item['stage']['numero']} "
+            
+        if self.item['user_email']['role'] in ("F", "T", "B", "A", "J", "V"):
+            self.button_detail_histo.text = f"{self.item['stage']['code']['code']} / #{self.item['stage']['numero']} "
 
     def button_detail_histo_click(self, **event_args):
         """This method is called when the button is clicked"""
